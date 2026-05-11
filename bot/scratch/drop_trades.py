8328e3f9b9d2af2247b0b7ad9564c83ada52e59c
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2026-05-11 17:07:21 -0400

    refactor: remove unused interface and implementation files

--- bot/scratch/drop_trades.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-cnx = mysql.connector.connect(user='root', host='localhost', password='')
-cursor = cnx.cursor()
-cursor.execute("DROP TABLE IF EXISTS `crypto-bot`.trades")
-cnx.commit()
-cursor.close()
-cnx.close()
-print("Table dropped")
